22a00a8348ca467f7a1f5e7af3f7d93eeab39b5f
Artur Shad Nik <arturshadnik@gmail.com>
2024-04-28 02:10:02 -0700

    WIP: add mostly working agent to generate and launch dashboard

--- backend/app/core/websocket.py
+++ backend/app/core/websocket.py
@@ -1,40 +1,54 @@
-from fastapi import WebSocket, WebSocketDisconnect, WebSocketException
+from fastapi import WebSocket
 from io import BytesIO
 import pandas as pd
+import os
 from time import time
+import subprocess
+import sys
 
 from app.core.genai.gemini_wrapper import start_gemini_session
 from app.core.database import db, storage
 from app.core.logger import logger
 
+ports = ["8501", "8502", "8503", "8504", "8505"]
 
+BASE_DATA_PATH="./_server_data"
+RUNNER_PATH="./launch_streamlit.sh"
 # store messages and a reference to the current and all past iterations
 # on close/save -> save whole state to db. when user tries to load past session, pull the whole state from db
 class WebSocketSession:
+
+    global ports
+
     def __init__(self, websocket: WebSocket) -> None:
         self.websocket = websocket
         self.created = int(time())
         self.gemini = start_gemini_session()
-        self.gemini_lite = start_gemini_session(lite=True)
         self.__db = db
         self.__blob = storage
         self.__revision_ids = []
-        self.__curr_revision = {}
-        self.__data = None
-        self.__code = None
+        self.__curr_revision = 0
+        self.__data: pd.DataFrame = None
+        self.__code = Code()
+        self.path = BASE_DATA_PATH + f"/{self.created}"
+        self.port = ports.pop(0)
+        self.process = None
+        os.makedirs(self.path, exist_ok=True)
+
 
     async def accept_connection(self) -> None:
         await self.websocket.accept()
 
     async def handle_disconnect(self) -> None:
+        self.process.terminate()
         await self._store_revision()
+        ports.append(self.port)
         await self.websocket.close()
 
     async def listen_and_serve(self) -> None:
         try:
             while True:
                 ws_data = await self.websocket.receive()
-                logger.info("Received message")
                 if "text" in ws_data:
                     data = ws_data["text"]
                     resp = await self._process_chat(data)
@@ -42,44 +56,19 @@ class WebSocketSession:
                     data = ws_data["bytes"]
                     resp = await self._process_file(data)
 
-                logger.info(resp)
                 if resp.startswith("```python"):
-                    self.__code = self._extract_code(resp)
-                    await self._validate_code()                        
-                    await self.websocket.send_text(self.__code) # replace with running deploy pipeline
+                    self.__code._extract(resp)
+                    await self.websocket.send_text(resp)
+                    await self.__code._validate(self.__data.head(n=2))
+                    self.__curr_revision += 1
+                    self._save_code()
+                    self._launch_dashboard()        
+                    await self.websocket.send_text(self.__code.code) # replace with running deploy pipeline
                 await self.websocket.send_text(resp)
         except Exception as e:
             logger.exception(e)
         finally:
             await self.handle_disconnect()
-
-    async def _validate_code(self) -> None:
-        errors = "untested"
-        while errors:
-            errors = self._test_code()
-            logger.warning(errors)
-            if errors:
-                resp = await self.gemini_lite.send_message_async(
-                    f"""
-                    This code:
-                    ```
-                    {self.__code}
-                    ```
-                    produced this error during execution: {errors}.
-                    Fix the code. Do not include anything else. No context or explanations. Only code enclosed in ```python<code>```
-                    """
-                )
-                self.__code = self._extract_code(resp.text)
-    
-    def _test_code(self) -> str | None:
-        try:
-            exec(self.__code)
-            return None
-        except Exception as e:
-            return str(e)
-
-    def _extract_code(self, text: str) -> str:
-        return text.removeprefix("```python").removesuffix("```")
 
     async def _process_chat(self, message: str) -> str:
         response = await self.gemini.send_message_async(message)
@@ -89,12 +78,18 @@ class WebSocketSession:
     async def _process_file(self, file: bytes) -> str:
         try:
             self.__data = pd.read_csv(BytesIO(file), on_bad_lines='skip')
-            preview = self.__data.head(n=10)
+            full_path = os.path.join(self.path, "data.csv")
+            os.environ["CSV_PATH"] = full_path
+
+            self.__data.to_csv(full_path)
+            preview = self.__data.head(n=5)
+
             response = await self.gemini.send_message_async(
                 f"""
-                Here is a preview of the data they provided: {preview}.
+                Here is a preview of the data they provided: {preview}. Do not mkae up new columns, only use what is in the data already.
                 """
             )
+            
             return response.text
         except pd.errors.ParserError as e:
             logger.exception(e)
@@ -104,9 +99,6 @@ class WebSocketSession:
             resp = f"Something went wrong: {e}"
         return resp
 
-
-    
-
     async def _store_revision(self) -> None:
         # upload to blob
         # store ref in db
@@ -115,3 +107,60 @@ class WebSocketSession:
 
     async def _load_revision(self):
         pass
+
+
+    def _launch_dashboard(self) -> None:
+        data_path = os.path.join(self.path, "data.csv")
+        dashboard_path = os.path.join(self.path, f"rev{self.__curr_revision}.py")
+        port = self.port
+        self.process = subprocess.Popen(['bash', RUNNER_PATH, data_path, dashboard_path, port])
+
+    def _save_code(self) -> None:    
+        out_path = os.path.join(self.path, f"rev{self.__curr_revision}.py")
+        with open(out_path, "w") as file:
+            file.write(self.__code.code)
+
+
+class Code:
+    def __init__(self) -> None:
+        self.code: str | None = None
+        self.gemini_lite = start_gemini_session(lite=True)
+        self.__errors: str | None = "untested"
+        self.__global_namespace = {
+            '__builtins': __builtins__,
+            'pandas': __import__('pandas'),
+            'streamlit': __import__('streamlit'),
+            'plotly': __import__('plotly'),
+            'matplotlib': __import__('matplotlib'),
+            'seaborn': __import__('seaborn')
+        }
+
+    async def _validate(self, preview: pd.DataFrame) -> None:
+        self._test()
+        while self.__errors:
+            logger.warning(self.__errors)
+            if self.__errors:
+                resp = await self.gemini_lite.send_message_async(
+                    f"""
+                    This code:
+                    ```
+                    {self.code}
+                    ```
+                    produced this error during execution: {self.__errors}.
+                    Fix the code. Do not include anything else. No context or explanations. Only the corrected 
+                    code enclosed in ```python<code>``` For the data path, always use the env var CSV_PATH=os.getenv("CSV_PATH).
+                    For reference heres a preview of their data {preview}. All columns in the dashboard must match these.
+                    """
+                )
+                self._extract(resp.text)
+                self._test()
+    
+    def _test(self) -> None:
+        try:
+            exec(self.code, self.__global_namespace)
+            self.__errors = None
+        except Exception as e:
+            self.__errors = e
+
+    def _extract(self, text: str) -> None:
+        self.code = text.removeprefix("```python").removesuffix("```")
