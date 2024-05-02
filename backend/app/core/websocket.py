b98f64915b009ea89327fbb663299975159087da
Artur Shad Nik <arturshadnik@gmail.com>
2024-05-02 12:13:22 -0700

    Add temp fix for streamlit ports

--- backend/app/core/websocket.py
+++ backend/app/core/websocket.py
@@ -2,15 +2,17 @@ from fastapi import WebSocket
 from io import BytesIO
 import pandas as pd
 import os
+import psutil
 from time import time
 import subprocess
 import json
+from time import sleep
 
 from app.core.genai.gemini_wrapper import start_gemini_session
 from app.core.database import db, storage
 from app.core.logger import logger
 
-ports = ["8501", "8502", "8503", "8504", "8505"]
+ports = list(range(8501, 8599))
 
 BASE_DATA_PATH="./_server_data"
 RUNNER_PATH="./launch_streamlit.sh"
@@ -34,12 +36,16 @@ class WebSocketSession:
         self.port = ports.pop(0)
         self.dash_process = None
         os.makedirs(self.path, exist_ok=True)
+
+        self.pid = None
  
     async def accept_connection(self) -> None:
         await self.websocket.accept()
 
     async def handle_disconnect(self) -> None:
+        logger.info("Terminating session")
         self._stop_dashboard()
+        self.__code.code = Code()
         await self._store_revision()
         ports.append(self.port)
         await self.websocket.close()
@@ -61,24 +67,30 @@ class WebSocketSession:
                         resp = "Invalid message format"
         
                     if resp.startswith("```python"):
+                        logger.info("got new code")
                         self.__code.extract(resp)
                         # await self.websocket.send_text(resp)
                         await self.__code.validate(self.__data.head(n=2))
+                        logger.info("validated")
                         # on updates, clean up the dashboard before relaunching
                         self._stop_dashboard()
+                        logger.info("Stopepd the old one")
+                        # if self.dash_process:
+                        #     self.dash_process.kill()
                         self.__curr_revision += 1
                         self._save_code()
                         self._launch_dashboard()
                         # await self.websocket.send_text(f'PORT:{self.port}')
-                        await self.websocket.send_json(data={"PORT":8501, "rev": self.__curr_revision})
+                        await self.websocket.send_json(data={"PORT":self.port, "rev": self.__curr_revision})
                         # await self.websocket.send_text(self.__code.code) # replace with running deploy pipeline
                     else: 
                         await self.websocket.send_text(resp)
                 except Exception as e:
-                    await self.websocket.send_text(f"I do know what went wrong. Ask me again!")
+                    await self.websocket.send_text(f"I don't know what went wrong. Ask me again!")
         except Exception as e:
             logger.exception(e)
         finally:
+            logger.info("Disconnecting")
             await self.handle_disconnect()
 
     async def _process_chat(self, message: str) -> str:
@@ -128,9 +140,12 @@ class WebSocketSession:
     def _launch_dashboard(self) -> None:
         base_path = self.path #self.path, "data.csv")
         dashboard_path = f"rev{self.__curr_revision}.py"  #os.path.join(self.path, f"rev{self.__curr_revision}.py")
-        port = self.port
+        port = str(self.port)
         cmd = ['bash', RUNNER_PATH, base_path, dashboard_path, port]
+        logger.info(f"starting {cmd}")
         self.dash_process = subprocess.Popen(cmd)
+        logger.info(self.dash_process.pid)
+        self.pid = self.dash_process.pid
 
     def _save_code(self) -> None:    
         out_path = os.path.join(self.path, f"rev{self.__curr_revision}.py")
@@ -151,8 +166,19 @@ class WebSocketSession:
     
     def _stop_dashboard(self) -> None:
         if self.dash_process:
-            self.dash_process.terminate()
-    
+            ports.append(self.port)
+            self.port = ports.pop(0)
+            logger.info(f"stopping {self.dash_process.pid}")
+            self._kill_process()
+
+
+    def _kill_process(self) -> None:
+        proc = psutil.Process(self.pid)
+        logger.info(proc)
+        proc.kill()
+        code = proc.wait(timeout=5)
+        logger.info(proc, code)
+
 class Code:
     def __init__(self) -> None:
         self.code: str | None = None
